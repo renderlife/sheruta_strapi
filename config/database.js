576e0b3e723ef8a45aa97d228a0130760b18c019
Emy Ifeora <emy@meetkumba.com>
2021-02-28 10:34:00 +0100

    ...

--- config/database.js
+++ config/database.js
@@ -1,18 +1,65 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'sheruta_strapi'),
-        username: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', '6625'),
-        ssl: env.bool('DATABASE_SSL', false),
+const parse = require('pg-connection-string').parse;
+
+module.exports = ({ env }) => {
+
+  if (env('NODE_ENV') === 'production') {
+    const config = parse(process.env.DATABASE_URL);
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'postgres',
+            host: config.host,
+            port: config.port,
+            database: config.database,
+            username: config.user,
+            password: config.password,
+          },
+          options: {
+            ssl: false,
+          },
+        },
       },
-      options: {}
+    }
+  }
+
+  return {
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'postgres',
+          host: env('DATABASE_HOST', '127.0.0.1'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'sheruta_strapi'),
+          username: env('DATABASE_USERNAME', 'postgres'),
+          password: env('DATABASE_PASSWORD', '6625'),
+          ssl: env.bool('DATABASE_SSL', false),
+        },
+        options: {}
+      },
     },
-  },
-});
+  };
+}
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'postgres',
+//         host: env('DATABASE_HOST', '127.0.0.1'),
+//         port: env.int('DATABASE_PORT', 5432),
+//         database: env('DATABASE_NAME', 'sheruta_strapi'),
+//         username: env('DATABASE_USERNAME', 'postgres'),
+//         password: env('DATABASE_PASSWORD', '6625'),
+//         ssl: env.bool('DATABASE_SSL', false),
+//       },
+//       options: {}
+//     },
+//   },
+// });
