ed2d0f9ce09b424d69fae339287432f235c27fde
Emy Ifeora <emy@meetkumba.com>
2021-02-28 01:27:21 +0100

    ..

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2ecbd36c743fccd69750ac5db2849fbd'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9470b222-a860-458e-a519-dc0de4e48f47'
+};
