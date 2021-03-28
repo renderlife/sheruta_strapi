d4386b554b8445db87b767de811ede9ac5db355a
Emy Ifeora <emy@meetkumba.com>
2021-03-28 21:36:11 +0100

    fixed personal info form

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -10,4 +10,24 @@
  * See more details here: https://strapi.io/documentation/developer-docs/latest/concepts/configurations.html#bootstrap
  */
 
-module.exports = () => {};
+module.exports = () => {
+    var io = require('socket.io')(strapi.server, {
+        cors: {
+            origin: "http://localhost:3000",
+            methods: ["GET", "POST"],
+            allowedHeaders: ["my-custom-header"],
+            credentials: true
+        }
+    });
+
+    io.on('connection', function (socket) {
+        socket.on('join', ({ username, room }) => {
+            console.log("user connected");
+            console.log("username is ", username);
+            console.log("room is...", room)
+        })
+        socket.on('emy', (data) => {
+            console.log('data -', data);
+        })
+    });
+};
